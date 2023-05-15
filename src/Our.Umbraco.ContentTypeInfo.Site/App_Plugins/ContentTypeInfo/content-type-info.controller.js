56930b3934d71df9366a74c6eebbff3d5ff4c18f
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-05-15 14:53:33 +0200

    Add content app

---
+++ src/Our.Umbraco.ContentTypeInfo.Site/App_Plugins/ContentTypeInfo/content-type-info.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    function contentTypeInfoController($scope, $routeParams) {
+
+        var vm = this;
+        vm.showInfo = true;
+
+        function init() {
+            vm.key = $scope.model.key;
+            vm.id = $scope.model.id;
+            vm.createDate = $scope.model.createDate;
+            vm.updateDate = $scope.model.updateDate;
+            vm.udi = $scope.model.udi;
+
+            if ($routeParams.create) {
+                vm.showInfo = false;                
+            }
+        }
+
+        init();
+    }
+
+    angular.module('umbraco')
+        .controller('Our.Umbraco.ContentTypeInfoController', contentTypeInfoController);
+})();
