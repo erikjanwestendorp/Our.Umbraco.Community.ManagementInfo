d063c1db09aeca5e207e665277b471ad7259064d
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-05-16 15:38:17 +0200

    Add Assets Project

---
+++ src/Our.Umbraco.ContentTypeInfo.Assets/Compose/UmbracoBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Our.Umbraco.ContentTypeInfo.Assets.ManifestFilters;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Our.Umbraco.ContentTypeInfo.Assets.Compose;
+
+public static class UmbracoBuilderExtensions
+{
+    public static IUmbracoBuilder ConfigureAssets(this IUmbracoBuilder builder)
+    {
+        if (builder.ManifestFilters().Has<ContentTypeInfoManifestFilter>())
+        {
+            return builder;
+        }
+
+        builder.ManifestFilters().Append<ContentTypeInfoManifestFilter>();
+
+        return builder;
+    }
+}
---
+++ src/Our.Umbraco.ContentTypeInfo.Assets/StaticAssetsBoot.cs
@@ -0,0 +1,13 @@
+﻿using Our.Umbraco.ContentTypeInfo.Assets.Compose;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Our.Umbraco.ContentTypeInfo.Assets;
+
+public class StaticAssetsBoot : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.ConfigureAssets();
+    }
+}
