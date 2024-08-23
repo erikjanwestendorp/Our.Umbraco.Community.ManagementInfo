88151d2a7d4720f45b8ea2f552f9df9d5a237325
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-08-23 14:25:30 +0200

    Get JSON

--- src/Our.Umbraco.Community.ManagementInfo.Assets/assets/src/my-element.ts
+++ src/Our.Umbraco.Community.ManagementInfo.Assets/assets/src/my-element.ts
@@ -1,25 +1,57 @@
 import { LitElement, html, customElement, css } from "@umbraco-cms/backoffice/external/lit";
 import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
+import { UMB_WORKSPACE_CONTEXT, UmbEntityWorkspaceContext } from "@umbraco-cms/backoffice/workspace";
+import { UmbDocumentTypeDetailRepository } from '@umbraco-cms/backoffice/document-type';
 
 @customElement('my-workspaceview')
 export default class MyWorspaceViewElement extends UmbElementMixin(LitElement) {
 
+    private documentTypeJson: string = '{}'; // Initialize with an empty JSON object
+
+    constructor() {
+        super();
+        console.log('data', this.dataset);
+
+        this.consumeContext(UMB_WORKSPACE_CONTEXT, async (ctx) => {
+            const context = ctx as UmbEntityWorkspaceContext;
+            const unique = context.getUnique();
+
+            if (unique) {
+                // Create an instance of the UmbDocumentTypeDetailRepository
+                const repository = new UmbDocumentTypeDetailRepository(this);
+
+                try {
+                    // Fetch the document type details using the repository
+                    const documentTypeDetails = await repository.requestByUnique(unique);
+
+                    if (documentTypeDetails) {
+                        // Convert the details to a JSON string with indentation
+                        this.documentTypeJson = JSON.stringify(documentTypeDetails, null, 2);
+                        this.requestUpdate(); // Request a re-render to display the JSON
+                    }
+                } catch (error) {
+                    console.error('Error fetching document type details:', error);
+                }
+            }
+        });
+    }
+
     render() {
         return html`     
-     <uui-box headline="Document Type Definition">
-       <umb-code-block language="JSON" copy>{}</umb-code-block>
-      </uui-box>    `
+        <uui-box headline="Document Type Definition">
+            <umb-code-block language="JSON" copy>${this.documentTypeJson}</umb-code-block>
+        </uui-box>`;
     }
 
     static styles = css`
     uui-box {
-      margin: 20px;
+        margin: 20px;
     }
-  `
+  `;
 }
 
 declare global {
     interface HTMLElementTagNameMap {
-        'my-workspaceview': MyWorspaceViewElement
+        'my-workspaceview': MyWorspaceViewElement;
     }
 }
--- src/Our.Umbraco.Community.ManagementInfo.Assets/wwwroot/App_Plugins/ManagementInfo/assets.js
+++ src/Our.Umbraco.Community.ManagementInfo.Assets/wwwroot/App_Plugins/ManagementInfo/assets.js
@@ -1,28 +1,44 @@
-import { LitElement as s, html as u, css as c, customElement as p } from "@umbraco-cms/backoffice/external/lit";
+import { LitElement as u, html as m, css as l, customElement as p } from "@umbraco-cms/backoffice/external/lit";
 import { UmbElementMixin as a } from "@umbraco-cms/backoffice/element-api";
-var b = Object.defineProperty, f = Object.getOwnPropertyDescriptor, v = (i, r, t, o) => {
-  for (var e = o > 1 ? void 0 : o ? f(r, t) : r, l = i.length - 1, m; l >= 0; l--)
-    (m = i[l]) && (e = (o ? m(r, t, e) : m(e)) || e);
-  return o && e && b(r, t, e), e;
+import { UMB_WORKSPACE_CONTEXT as y } from "@umbraco-cms/backoffice/workspace";
+import { UmbDocumentTypeDetailRepository as f } from "@umbraco-cms/backoffice/document-type";
+var d = Object.defineProperty, h = Object.getOwnPropertyDescriptor, b = (r, n, t, o) => {
+  for (var e = o > 1 ? void 0 : o ? h(n, t) : n, s = r.length - 1, i; s >= 0; s--)
+    (i = r[s]) && (e = (o ? i(n, t, e) : i(e)) || e);
+  return o && e && d(n, t, e), e;
 };
-let n = class extends a(s) {
+let c = class extends a(u) {
+  // Initialize with an empty JSON object
+  constructor() {
+    super(), this.documentTypeJson = "{}", console.log("data", this.dataset), this.consumeContext(y, async (r) => {
+      const t = r.getUnique();
+      if (console.log(t), t) {
+        const o = new f(this);
+        try {
+          const e = await o.requestByUnique(t);
+          e && (this.documentTypeJson = JSON.stringify(e, null, 2), this.requestUpdate());
+        } catch (e) {
+          console.error("Error fetching document type details:", e);
+        }
+      }
+    });
+  }
   render() {
-    return u`     
-     <uui-box headline="Document Type Definition">
-       <umb-code-block language="JSON" copy>{}</umb-code-block>
-      </uui-box>            
-    `;
+    return m`     
+        <uui-box headline="Document Type Definition">
+            <umb-code-block language="JSON" copy>${this.documentTypeJson}</umb-code-block>
+        </uui-box>`;
   }
 };
-n.styles = c`
+c.styles = l`
     uui-box {
-      margin: 20px;
+        margin: 20px;
     }
   `;
-n = v([
+c = b([
   p("my-workspaceview")
-], n);
+], c);
 export {
-  n as default
+  c as default
 };
 //# sourceMappingURL=assets.js.map
